25a4d7aafe09cdba33fddf10fc74cb7eb7132249
Emilio Vesprini <emivespa1@gmail.com>
2023-05-15 21:58:39 +0000

    Revert "use mdx"
    This reverts commit d373ae33cab2dd068a85245eee8dcef9088ea44b.

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,15 @@
-import ReadMe from '@/README.mdx'
-
 export default function Home() {
   return (
-    <div className="markdown m-4">
-      <ReadMe />
-    </div>
+    <>
+      <main className="">
+        <div className="m-4 markdown">
+          <h1>fullfatsearch</h1>
+          <p>es un motor de busqueda para episodios de @CirculoVicioso8</p>
+          <h2>dorking</h2>
+          <pre>videoId:MIoxw3nupFo AND &quot;propio marco&quot;</pre>
+          <pre>&quot;mercado como&quot; AND (concepto OR entidad) </pre>
+        </div>
+      </main>
+    </>
   )
-  // return (
-  //   <>
-  //     <main className="">
-  //       <div className="m-4 markdown">
-  //         <h1>fullfatsearch</h1>
-  //         <p>es un motor de busqueda para episodios de @CirculoVicioso8</p>
-  //         <h2>dorking</h2>
-  //         <pre>videoId:MIoxw3nupFo AND &quot;propio marco&quot;</pre>
-  //         <pre>&quot;mercado como&quot; AND (concepto OR entidad) </pre>
-  //       </div>
-  //     </main>
-  //   </>
-  // )
 }
