d373ae33cab2dd068a85245eee8dcef9088ea44b
Emilio Vesprini <emivespa1@gmail.com>
2023-05-15 21:39:30 +0000

    use mdx

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,22 @@
+import ReadMe from '@/README.mdx'
+
 export default function Home() {
   return (
-    <>
-      <main className="">
-        <div className="m-4 markdown">
-          <h1>fullfatsearch</h1>
-          <p>es un motor de busqueda para episodios de @CirculoVicioso8</p>
-          <h2>dorking</h2>
-          <pre>videoId:MIoxw3nupFo AND &quot;propio marco&quot;</pre>
-          <pre>&quot;mercado como&quot; AND (concepto OR entidad) </pre>
-        </div>
-      </main>
-    </>
+    <div className="markdown m-4">
+      <ReadMe />
+    </div>
   )
+  // return (
+  //   <>
+  //     <main className="">
+  //       <div className="m-4 markdown">
+  //         <h1>fullfatsearch</h1>
+  //         <p>es un motor de busqueda para episodios de @CirculoVicioso8</p>
+  //         <h2>dorking</h2>
+  //         <pre>videoId:MIoxw3nupFo AND &quot;propio marco&quot;</pre>
+  //         <pre>&quot;mercado como&quot; AND (concepto OR entidad) </pre>
+  //       </div>
+  //     </main>
+  //   </>
+  // )
 }
